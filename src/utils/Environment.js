a18b5e36da0497ffc1419282f7bd0c5d8f3c13b7
waleed-shafiq <waleed-shafiq96@gmail.com>
2021-10-15 20:15:02 +0500

    deploy wiht dynamic price

--- src/utils/Environment.js
+++ src/utils/Environment.js
@@ -1,5 +1,5 @@
 let Environment = {
-    kingsAddress: "0xcB16Dc5048E915B9e301b0A02BFd30CEE49f9816",
-  };
-  
-  export default Environment;
+  kingsAddress: "0xcB16Dc5048E915B9e301b0A02BFd30CEE49f9816",
+};
+
+export default Environment;
